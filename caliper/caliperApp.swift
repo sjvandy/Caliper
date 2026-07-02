a55900cbbca05c0ce8aeb2102aa329a37147b75b
Steven Vandegrift <svandegrift@icloud.com>
2026-07-01 23:35:20 -0700

    Initial Commit

---
+++ caliper/caliperApp.swift
@@ -0,0 +1,17 @@
+//
+//  caliperApp.swift
+//  caliper
+//
+//  Created by Steven Vandegrift on 7/1/26.
+//
+
+import SwiftUI
+
+@main
+struct caliperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
